57085aaf8f5ca175fb0056b8574f888435bed8ff
Antoinette Janus <Antoinette Janus>
2017-04-25 23:36:30 -0400

    Initial commit - React and Electron work together - Webpack config - Reach starter

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello world!</div>
+    )
+  }
+}
+
+const app = document.getElementById('app')
+
+ReactDOM.render(<App/>, app);

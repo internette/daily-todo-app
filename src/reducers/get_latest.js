9e45c72e551248a4202933a2f8f7672b1b82e803
Antoinette <acjanus@Bobs-Mac-mini.local>
2017-05-15 11:06:48 -0400

    Fixed broken id assignment for new items

--- src/reducers/get_latest.js
+++ src/reducers/get_latest.js
@@ -1,10 +1,10 @@
 export const latestStats = (state = [], action) => {
   switch (action.type) {
     case 'init':
-      return {
+      return Object.assign({}, state, {
         resetDate: action.resetDate,
         nextId: action.nextId
-      }
+      })
     default:
       return state
   }
